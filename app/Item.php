47773254cf0a0b38c8b2e679d3693bd673354a3b
mtotodev05 <mtotodev05@gmail.com>
2018-03-27 04:45:38 +0300

    Implemented Model

--- app/Item.php
+++ app/Item.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+    protected $fillable =[
+        'item_name', 'description','category','condition','start_price','minimum_price'
+        ,'end_day','inst_price', 'returns'
+    ];
+
+    public function user(){
+        $this->belongsTo(User::class);
+    }
 }
