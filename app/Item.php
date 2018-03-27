250449dd0f2b9d656069e54a514fdaed08389e01
mtotodev05 <mtotodev05@gmail.com>
2018-03-27 05:02:29 +0300

    Implemented Model

--- app/Item.php
+++ app/Item.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     protected $fillable =[
-        'item_name', 'description','category','condition','start_price','minimum_price'
+        'user_id','item_name', 'description','category','condition','start_price','minimum_price'
         ,'end_day','inst_price', 'returns'
     ];
 
--- database/migrations/2018_03_26_232957_create_items_table.php
+++ database/migrations/2018_03_26_232957_create_items_table.php
@@ -15,6 +15,16 @@ class CreateItemsTable extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('item_name');
+            $table->text('description');
+            $table->string('category');
+            $table->string('condition');
+            $table->double('start_price');
+            $table->double('minimum_price');
+            $table->string('end_day');
+            $table->double('inst_price');
+            $table->string('returns');
             $table->timestamps();
         });
     }
